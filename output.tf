1608389dfb28e8dbcf81f25e299464f2262b25b4
rajangold02 <rajangold02@gmail.com>
2018-09-04 06:51:28 -0400

    changes

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "vcp_id" {
 	value = "${aws_vpc.my_vpc.id}"
 }
-output "subnet_id" {
+output "subnet_public_id" {
 	value = "${aws_subnet.my_public_subnet.id}"
 }
-output "subnet_id" {
+output "subnet_private_id" {
 	value = "${aws_subnet.my_private_subnet.id}"
 }
